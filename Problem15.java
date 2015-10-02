132da9ee94ef53ac2005d28d374883de5c33258c
zbruennig <zkb@ku.edu>
2015-10-02 14:54:55 -0500

    Removed irrelevant files
    These either didn't have any code or weren't problems from Project Euler

--- Problem15.java
+++
@@ -1,8 +0,0 @@
-public class Problem15
-{
-	/*
-	 * Problem 15 is a binomial distribution problem
-	 * 40 trials, 20 success, .5 probability of success on a single trial
-	 * Take that decimal and multiply it by 2^40 to get the answer
-	 */
-}
